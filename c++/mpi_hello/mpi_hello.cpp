984330e4a73986fd28c74ce6538e0ea4682bcd08
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-10-29 18:06:52 -0500

    Add updated version

--- c++/mpi_hello/mpi_hello.cpp
+++ c++/mpi_hello/mpi_hello.cpp
@@ -1,13 +1,23 @@
 #include <mpi.h>
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
+using namespace MPI;
 
 int main(int argc, char *argv[]){
-
+    
+    string greeting;
+    // Initialize MPI.
     MPI::Init();
+
+    // Get the individual process ID
     int rank = MPI::COMM_WORLD.Get_rank();
+
+    // Get the number of processes
     int size = MPI::COMM_WORLD.Get_size();
+
     int len; char name[MPI_MAX_PROCESSOR_NAME];
 
     MPI::Get_processor_name(name, len);
